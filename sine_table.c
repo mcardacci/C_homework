68f70abbce7c56f8229d89b2ec8ace457d5ae143
Marco Cardacci <marcoc@ticketech.local>
2016-07-15 14:17:23 -0400

    adds sine table

---
+++ sine_table.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+    int angle_degree;
+    double angle_radian, pi, value;
+
+    printf("\nCompute a table of the sine function\n\n");
+
+    pi = 4.0*atan(1.0);
+    printf("Value of PI = %f \n\n", pi);
+
+    printf("angle        Sine\n");
+
+    angle_degree=0;
+
+    while(angle_degree <= 360)
+    {
+        angle_radian = pi * angle_degree/180.0;
+        value = sin(angle_radian);
+        printf("%3d         %f\n", angle_degree, value);
+
+        angle_degree = angle_degree + 10;
+    }
+
+}
