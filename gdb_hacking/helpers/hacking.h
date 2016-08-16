8696983bf8076cf1e142fed2263aabb3e014abef
Marco Cardacci <marco.cardacci@gmail.com>
2016-08-15 20:34:32 -0400

    tiny web

--- gdb_hacking/helpers/hacking.h
+++ gdb_hacking/helpers/hacking.h
@@ -38,7 +38,7 @@ void dump(const unsigned char *data_buffer, const unsigned int length)
         {
             for (j = 0; j < 15-(i%16); j++) 
             {
-                printf("    ");
+                printf("   ");
             }
             printf("| ");
             for (j = (i-(i%16)); j <= i; j++)
@@ -49,9 +49,11 @@ void dump(const unsigned char *data_buffer, const unsigned int length)
                     printf("%c", byte);
                 }
                 else
+                {
                     printf(".");
+                }
             }
             printf("\n"); // End of the dump line (each line is 16 bytes)
         } // End of if
     } // End of for
-}
+} 
